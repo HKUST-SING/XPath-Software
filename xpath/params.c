756745c96bbeecd02cf67bb69908eb8a1f574c78
wei bai <baiwei0427@sina.com>
2016-05-03 21:32:15 +0800

    Fix a bug of sysctl
    We should use pointers for extra1/extra2.

--- xpath/params.c
+++ xpath/params.c
@@ -4,12 +4,17 @@
 #include "params.h"
 
 int xpath_load_balancing = ECMP;
+int xpath_load_balancing_min = ECMP;
+int xpath_load_balancing_max = RPS;
+
 int xpath_enable_debug = 0;
+int xpath_enable_debug_min = 0;
+int xpath_enable_debug_max = 1;
 
 struct xpath_param xpath_params[2] =
 {
 	{"load_balancing", &xpath_load_balancing},
-    {"enable_debug", &xpath_enable_debug},
+	{"enable_debug", &xpath_enable_debug},
 };
 
 struct ctl_table xpath_params_table[2];
@@ -33,8 +38,8 @@ bool xpath_params_init(void)
     entry->data = xpath_params[0].ptr;
     entry->mode = 0644;
 	entry->proc_handler = &proc_dointvec_minmax;
-	entry->extra1 = ECMP;
-    entry->extra2 = RPS;
+	entry->extra1 = &xpath_load_balancing_min;
+    entry->extra2 = &xpath_load_balancing_max;
     entry->maxlen=sizeof(int);
 
     /* xpath.enable_debug */
@@ -43,8 +48,8 @@ bool xpath_params_init(void)
     entry->data = xpath_params[1].ptr;
     entry->mode = 0644;
     entry->proc_handler = &proc_dointvec_minmax;
-    entry->extra1 = 0;
-    entry->extra2 = 1;
+    entry->extra1 = &xpath_enable_debug_min;
+    entry->extra2 = &xpath_enable_debug_max;
     entry->maxlen=sizeof(int);
 
     xpath_sysctl = register_sysctl_paths(xapath_params_path, xpath_params_table);

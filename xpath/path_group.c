a99821c1b69123b8b163f9029de0b0e2bf6bc543
wei bai <baiwei0427@sina.com>
2016-12-23 21:01:03 +0800

    Update per-path state

--- xpath/path_group.c
+++ xpath/path_group.c
@@ -8,12 +8,18 @@ bool xpath_init_path_group(struct path_group_entry *pg, unsigned int size)
                 return false;
 
         for (i = 0; i < size; i++) {
-                pg[i].ecn_fraction = 0;
-                pg[i].bytes_sent = 0;
+                pg[i].avg_ecn_fraction = 0;
+                pg[i].bytes_acked = 0;
                 pg[i].bytes_ecn = 0;
-                pg[i].rate_avg = 0;
-                pg[i].rtt_avg = 0;
-                pg[i].start_time = ktime_set(0, 0);
+                pg[i].last_ecn_update_time = ktime_set(0, 0);
+
+                pg[i].smooth_rtt_us = 100;
+                pg[i].last_rtt_update_time = ktime_set(0, 0);
+
+                pg[i].bytes_sent = 0;
+                pg[i].smooth_rate_mbps = 0;
+                pg[i].last_rate_update_time = ktime_set(0, 0);
+
                 spin_lock_init(&(pg[i].lock));
         }
 
--- xpath/path_group.h
+++ xpath/path_group.h
@@ -5,12 +5,21 @@
 #include <linux/ktime.h>
 
 struct path_group_entry {
-        unsigned int ecn_fraction;
-        unsigned int bytes_sent;
-        unsigned int bytes_ecn;
-        unsigned short rate_avg;
-        unsigned short rtt_avg;
-        ktime_t start_time;
+        /* ECN information */
+        u16 avg_ecn_fraction;   //average ECN fraction of all flows (maximum 1024)
+        u32 bytes_acked; //bytes ACKed in this path
+        u32 bytes_ecn;  //bytes get ECN marked in this path
+        ktime_t last_ecn_update_time;   //last time when we update ECN statistic
+
+        /* RTT information */
+        u16 smooth_rtt_us;    //smooth RTT in microsecond of this path
+        ktime_t last_rtt_update_time;   //last time when we update RTT information
+
+        /* sending rate information */
+        u32 bytes_sent; //bytes sent in this path
+        u16 smooth_rate_mbps;   //average sending rate in Mbps
+        ktime_t last_rate_update_time;   //last time when we update rate information
+
         spinlock_t lock;
 };
 
